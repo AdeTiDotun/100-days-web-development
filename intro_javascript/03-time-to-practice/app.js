bc0dac4cf27542c18f566bf0b2cf5449f7c6316d
AdeTiDotun <adejumodaniel274@gmail.com>
2023-10-24 14:49:25 +0100

    event listener

---
+++ intro_javascript/03-time-to-practice/app.js
@@ -0,0 +1,9 @@
+let paragraphElement = document.querySelector('p');
+
+
+function changeElement() {
+  paragraphElement.textContent = 'Clicked';
+  console.log ('clicked');
+}
+
+paragraphElement.addEventListener ('click', changeElement);
