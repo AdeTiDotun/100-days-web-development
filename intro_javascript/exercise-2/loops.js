eae8bf79871394302056d84d5820b155c62d9485
AdeTiDotun <adejumodaniel274@gmail.com>
2023-11-16 21:36:36 +0100

    for...of statement

--- intro_javascript/exercise-2/loops.js
+++ intro_javascript/exercise-2/loops.js
@@ -5,3 +5,11 @@ for (i = 0; i <= 10; i++) {
     console.log (i);
 }
 
+// for...of statement; creates a loop, ilterating over ilterable objects like array, map, set, arguments etc.
+
+const myObjects = ["Dan", "Developer", "Hustle", "Grind"];
+
+for (const storeArray of myObjects) {
+    console.log (storeArray);
+}
+
