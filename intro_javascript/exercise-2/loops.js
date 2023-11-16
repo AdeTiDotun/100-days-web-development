45a3b7a9d91fc55c25df54759c395c1c2b42972c
AdeTiDotun <adejumodaniel274@gmail.com>
2023-11-16 21:29:17 +0100

    for statement

---
+++ intro_javascript/exercise-2/loops.js
@@ -0,0 +1,7 @@
+// for statement; repeats until a specified condition evalutes to false.
+// helpful whenever a random number is needed to be generated.
+
+for (i = 0; i <= 10; i++) {
+    console.log (i);
+}
+
