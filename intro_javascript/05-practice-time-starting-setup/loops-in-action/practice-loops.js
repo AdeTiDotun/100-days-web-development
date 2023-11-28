89918f2e2b05aec69a0ffcd0e17d99911446465d
AdeTiDotun <adejumodaniel274@gmail.com>
2023-11-28 21:56:52 +0100

    made a few changes with the first question on practice.js file

---
+++ intro_javascript/05-practice-time-starting-setup/loops-in-action/practice-loops.js
@@ -0,0 +1,14 @@
+// First question
+
+calculationButton = document.querySelector('#calculator button');
+
+function calculatedSum() {
+    userNumber = document.getElementById('user-number');
+    
+
+}
+
+calculationButton.addEventListener('click', calculatedSum);
+
+
+
